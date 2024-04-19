45c69839d6ca8767c702e076e764969b45d8eaa1
Julian Bartosz <thejulianbartosz@gmail.com>
2024-04-19 11:38:29 -0500

    Delete user_management directory

--- user_management/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserManagementConfig(AppConfig):
-    name = 'user_management'
-    verbose_name = 'User Management'
-
-    def ready(self):
-        import user_management.signals
--- user_management/signals.py
+++
@@ -1,7 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from .models import User
-
-
-
