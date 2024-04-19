2c81f120af2597561cd5d585cf522e99b07e15da
Cainan Squibb <cgsquibb@uwm.edu>
2024-04-19 18:36:49 -0500

    Edit course HTML page, view, tests, form, and url.  Also create course test, HTML page, form, view, url

--- TAScheduler/views.py
+++ TAScheduler/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import UpdateView
+
+from TAScheduler.forms import CreateUserForm, MyUserUpdateForm
+from TAScheduler.models import MyUser
+
 from TAScheduler.models import MyUser
 from TAScheduler.classes import Auth
 
@@ -23,3 +29,26 @@ class Homepage(View):
 
     def post(self, request):
         return render(request, "homepage.html")
+
+class EditAccountView(UpdateView):
+    model = MyUser
+    form_class = MyUserUpdateForm
+    template_name = 'user_management/edit_account.html'
+    success_url = reverse_lazy('home')  # Update with your success URL
+
+    def get_object(self):
+        return self.request.user
+
+def CreateUserView(request):
+    def create_user(request):
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('user_created')
+        else:
+            form = CreateUserForm()
+        return render(request, 'create_user.html', {'form': form})
+
+    def user_created(request):
+        return render(request, 'templates/HomePages/AdminHome.html')
