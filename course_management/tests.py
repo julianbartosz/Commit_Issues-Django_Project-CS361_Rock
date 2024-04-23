545e327daef2bf9e646732717de9f2423e41b35a
Julian Bartosz <thejulianbartosz@gmail.com>
2024-04-23 07:41:48 -0500

    implemented course_management/tests.py (all pass)
    worked out comprehensive unit testing of course_management app w/out documentation

--- course_management/tests.py
+++ course_management/tests.py
@@ -1,31 +1,282 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from course_management.models import Course
-# Create your tests here.#t
+from user_management.models import User
+from course_management.forms import CourseForm
+from django.contrib.auth.models import AnonymousUser
+from django.urls import reverse, resolve
+from course_management.views import CourseDetailView, CourseListView, CourseCreateView, CourseUpdateView, CourseDeleteView
+from django.contrib.admin.sites import AdminSite
+from course_management.admin import CourseAdmin
 
 
-class CourseTestCase(TestCase):
-  def setUp(self):
-    self.course = Course.objects.create(title="Math",
-                                        description="math is good",
-                                        instructor="rock",
-                                        requirements="nothing")
+class CourseModelTest(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(email='instructor@test.com', password='testpassword')
+        self.user2 = User.objects.create(email='ta1@test.com', password='testpassword')
+        self.user3 = User.objects.create(email='ta2@test.com', password='testpassword')
 
-  def test_course_creation(self):
-    # Test creating a new Course instance
-    self.assertEqual(self.course.title, "Math")
-    self.assertEqual(self.course.description, "math is good")
-    self.assertEqual(self.course.instructor, "rock")
-    self.assertEqual(self.course.requirements, "nothing")
+    def test_create_course_with_all_fields(self):
+        course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user1,
+            semester='Fall',
+            year=2022
+        )
+        course.tas.add(self.user2, self.user3)
+        self.assertEqual(course.code, 'CS101')
+        self.assertEqual(course.title, 'Intro to CS')
+        self.assertEqual(course.description, 'This is a test course.')
+        self.assertEqual(course.instructor, self.user1)
+        self.assertEqual(list(course.tas.all()), [self.user2, self.user3])
+        self.assertEqual(course.semester, 'Fall')
+        self.assertEqual(course.year, 2022)
 
-  def test_class_already_stored(self):
-    # Check if the class is already stored in the database
-    self.course = Course.objects.create(title="Math",
-                                        description="lol",
-                                        instructor="adas",
-                                        requirements="kmvds")
-    course_exists = Course.objects.filter(title="Math",
-                                          description="lol",
-                                          instructor="adas",
-                                          requirements="kmvds").exists()
-    self.assertTrue(course_exists)
+    def test_str_method(self):
+        course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user1,
+            semester='Fall',
+            year=2022
+        )
+        self.assertEqual(str(course), 'CS101 - Intro to CS')
 
+    def test_code_unique(self):
+        Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user1,
+            semester='Fall',
+            year=2022
+        )
+        with self.assertRaises(Exception):
+            Course.objects.create(
+                code='CS101',
+                title='Intro to CS 2',
+                description='This is another test course.',
+                instructor=self.user1,
+                semester='Fall',
+                year=2022
+            )
+
+    def test_instructor_can_be_none(self):
+        course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=None,
+            semester='Fall',
+            year=2022
+        )
+        self.assertIsNone(course.instructor)
+
+    def test_tas_can_have_multiple_users(self):
+        course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user1,
+            semester='Fall',
+            year=2022
+        )
+        course.tas.add(self.user2, self.user3)
+        self.assertEqual(list(course.tas.all()), [self.user2, self.user3])
+
+
+class CourseDetailViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@test.com', password='testpassword')
+        self.course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user,
+            semester='Fall',
+            year=2022
+        )
+
+    def test_get_context_data(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        view = CourseDetailView()
+        view.setup(request, pk=self.course.pk)
+        view.get(request, pk=self.course.pk)
+        context = view.get_context_data()
+        self.assertEqual(context['course'], self.course)
+
+
+class CourseListViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@test.com', password='testpassword')
+        self.course1 = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.user,
+            semester='Fall',
+            year=2022
+        )
+        self.course2 = Course.objects.create(
+            code='CS102',
+            title='Intro to Python',
+            description='This is another test course.',
+            instructor=self.user,
+            semester='Fall',
+            year=2022
+        )
+
+    def test_get_queryset(self):
+        request = self.factory.get('/', {'search': 'Python'})
+        request.user = self.user
+        view = CourseListView()
+        view.setup(request)
+        queryset = view.get_queryset()
+        self.assertEqual(list(queryset), [self.course2])
+
+
+class CourseCreateViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@test.com', password='testpassword', role='Supervisor')
+
+    def test_test_func(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        view = CourseCreateView()
+        view.setup(request)
+        self.assertTrue(view.test_func())
+
+
+class CourseUpdateViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@test.com', password='testpassword', role='Supervisor')
+
+    def test_test_func(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        view = CourseUpdateView()
+        view.setup(request)
+        self.assertTrue(view.test_func())
+
+
+class CourseDeleteViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create(email='test@test.com', password='testpassword', role='Supervisor')
+
+    def test_test_func(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        view = CourseDeleteView()
+        view.setup(request)
+        self.assertTrue(view.test_func())
+
+
+class CourseFormTest(TestCase):
+    def setUp(self):
+        self.instructor = User.objects.create(email='instructor@test.com', password='testpassword', role='Instructor')
+        self.ta = User.objects.create(email='ta@test.com', password='testpassword', role='TA')
+
+    def test_form_with_valid_data(self):
+        form = CourseForm(data={
+            'code': 'CS101',
+            'title': 'Intro to CS',
+            'description': 'This is a test course.',
+            'instructor': self.instructor.id,
+            'tas': [self.ta.id],
+            'semester': 'Fall',
+            'year': 2022
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_form_with_missing_fields(self):
+        form = CourseForm(data={})
+        self.assertFalse(form.is_valid())
+
+    def test_clean_code_with_non_alphanumeric_code(self):
+        form = CourseForm(data={
+            'code': 'CS-101',
+            'title': 'Intro to CS',
+            'description': 'This is a test course.',
+            'instructor': self.instructor.id,
+            'tas': [self.ta.id],
+            'semester': 'Fall',
+            'year': 2022
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['code'], ['Course code must be alphanumeric.'])
+
+    def test_clean_year_with_year_less_than_1900(self):
+        form = CourseForm(data={
+            'code': 'CS101',
+            'title': 'Intro to CS',
+            'description': 'This is a test course.',
+            'instructor': self.instructor.id,
+            'tas': [self.ta.id],
+            'semester': 'Fall',
+            'year': 1899
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['year'], ['Ensure this value is greater than or equal to 1900.'])
+
+
+class CourseURLsTest(TestCase):
+    def test_course_list_url_resolves(self):
+        url = reverse('course_management:course_list')
+        self.assertEqual(resolve(url).func.view_class, CourseListView)
+
+    def test_course_create_url_resolves(self):
+        url = reverse('course_management:course_create')
+        self.assertEqual(resolve(url).func.view_class, CourseCreateView)
+
+    def test_course_detail_url_resolves(self):
+        url = reverse('course_management:course_detail', args=[1])
+        self.assertEqual(resolve(url).func.view_class, CourseDetailView)
+
+    def test_course_update_url_resolves(self):
+        url = reverse('course_management:course_update', args=[1])
+        self.assertEqual(resolve(url).func.view_class, CourseUpdateView)
+
+    def test_course_delete_url_resolves(self):
+        url = reverse('course_management:course_delete', args=[1])
+        self.assertEqual(resolve(url).func.view_class, CourseDeleteView)
+
+
+class CourseAdminTest(TestCase):
+    def setUp(self):
+        self.site = AdminSite()
+        self.admin = CourseAdmin(Course, self.site)
+        self.instructor = User.objects.create(email='instructor@test.com', password='testpassword', role='Instructor')
+        self.ta1 = User.objects.create(email='ta1@test.com', password='testpassword', role='TA')
+        self.ta2 = User.objects.create(email='ta2@test.com', password='testpassword', role='TA')
+        self.course = Course.objects.create(
+            code='CS101',
+            title='Intro to CS',
+            description='This is a test course.',
+            instructor=self.instructor,
+            semester='Fall',
+            year=2022
+        )
+        self.course.tas.add(self.ta1, self.ta2)
+
+    def test_list_display(self):
+        self.assertEqual(self.admin.list_display, ('code', 'title', 'instructor_name', 'number_of_tas'))
+
+    def test_search_fields(self):
+        self.assertEqual(self.admin.search_fields, ('code', 'title', 'instructor__username', 'tas__username'))
+
+    def test_list_filter(self):
+        self.assertEqual(self.admin.list_filter, ('instructor', 'tas'))
+
+    def test_instructor_name(self):
+        self.assertEqual(self.admin.instructor_name(self.course), 'instructor@test.com')
+
+    def test_number_of_tas(self):
+        self.assertEqual(self.admin.number_of_tas(self.course), 2)
