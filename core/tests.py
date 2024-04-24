64a86f2f51c97f59d7c00b1afe220bc2448dbfb0
Julian Bartosz <thejulianbartosz@gmail.com>
2024-04-24 08:49:28 -0500

    Updated tests.py (unit + acceptance)

--- core/tests.py
+++ core/tests.py
@@ -6,43 +6,7 @@ from django import forms
 from core.views import home, search_select
 from core.forms import CustomAuthenticationForm
 
-
-class HomeViewTest(TestCase):
-    """
-    This class contains tests for the home view.
-    """
-    def setUp(self):
-        """
-        This method sets up the test case with a client. It is run before each test method.
-        """
-        self.client = Client()
-
-    def test_home_view(self):
-        """
-        This test checks the home view. It asserts that the response status code is 200 and the correct template is used.
-        """
-        response = self.client.get(reverse('home'))  # Replace 'home' with the actual name of the URL pattern for the home view
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/home.html')
-
-
-class SearchSelectViewTest(TestCase):
-    """
-    This class contains tests for the search_select view.
-    """
-    def setUp(self):
-        """
-        This method sets up the test case with a client. It is run before each test method.
-        """
-        self.client = Client()
-
-    def test_search_select_view(self):
-        """
-        This test checks the search_select view. It asserts that the response status code is 200 and the correct template is used.
-        """
-        response = self.client.get(reverse('search_select'))  # Replace 'search_select' with the actual name of the URL pattern for the search_select view
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/search_select.html')
+# UNIT TESTS
 
 
 class CustomAuthenticationFormTest(TestCase):
@@ -95,3 +59,46 @@ class UrlsTest(TestCase):
         """
         url = reverse('search_select')
         self.assertEqual(resolve(url).func, search_select)
+
+# UNIT TESTS END
+# ACCEPTANCE TESTS
+
+
+class HomeViewTest(TestCase):
+    """
+    This class contains tests for the home view.
+    """
+    def setUp(self):
+        """
+        This method sets up the test case with a client. It is run before each test method.
+        """
+        self.client = Client()
+
+    def test_home_view(self):
+        """
+        This test checks the home view. It asserts that the response status code is 200 and the correct template is used.
+        """
+        response = self.client.get(reverse('home'))  # Replace 'home' with the actual name of the URL pattern for the home view
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/home.html')
+
+
+class SearchSelectViewTest(TestCase):
+    """
+    This class contains tests for the search_select view.
+    """
+    def setUp(self):
+        """
+        This method sets up the test case with a client. It is run before each test method.
+        """
+        self.client = Client()
+
+    def test_search_select_view(self):
+        """
+        This test checks the search_select view. It asserts that the response status code is 200 and the correct template is used.
+        """
+        response = self.client.get(reverse('search_select'))  # Replace 'search_select' with the actual name of the URL pattern for the search_select view
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'core/search_select.html')
+
+# ACCEPTANCE TESTS END
